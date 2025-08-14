408f232cf6c59ad337fef1af9521022f65a43d14
Gustavo627 <gbonfim411@gmail.com>
2025-08-14 16:28:43 -0300

    awredtguiokpl

---
+++ aulas/back/exercícios de fixação/exs.js
@@ -0,0 +1,38 @@
+//EXERCÍCIO 1
+
+ console.log("Meu nome é Gustavo.")
+ console.log("Eu tenho  17 anos.")
+
+ //EXERCÍCIO 2
+
+const hoje = "Hoje"
+const esta =  "está"
+const chuva = "chovendo."
+
+console.log(hoje)
+console.log(esta)
+console.log(chuva)
+
+console.log(hoje, esta, chuva)
+
+//EXERCÍCIO 3
+
+const curso = "curso"
+const modulo = "módulo"
+const aulas = "aulas"
+
+console.log(curso + modulo + aulas)
+
+//EXERCÍCIO 4
+
+console.log("1 2")
+console.log(1, 2) 
+
+//EXERCÍCIO 5
+
+/*const nome = prompt("Digite seu nome: ")
+alert("oi")*/
+
+//EXERCÍCIO 6
+
+confirm("quer receber novidades?")
