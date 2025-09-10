c162f0b3a70d2cdac29341c7769cb4017bbe1779
Gustavo627 <gbonfim411@gmail.com>
2025-09-10 16:29:22 -0300

    a

--- aulas/back/CRIACAOAPI/minha-primeira-api/server.js
+++ aulas/back/CRIACAOAPI/minha-primeira-api/server.js
@@ -21,20 +21,55 @@ let usuarios = [
     {id: 1, nome: "Ana", idade: 25},
     {id: 2, nome: "Carlos", idade: 30},
     {id: 3, nome: "Maria", idade: 22},
-    {id: 4, nome: "Marcos", idade: 18}
+    {id: 4, nome: "Marcos", idade: 18},
+    {id: 5, nome: "Ana Júlia", idade: 15}
 ]
 
 app.get('/',(require, response) => {
     response.send("TESTE")
 })
 
-app.get('/usuarios',(require, response) =>{
+app.get('/usuarios',(request, response) =>{
     response.json(usuarios);
 })
 
-app.get('/usuarios/:id', (require, response) =>{
-    const id = require.params.id
+
+app.get('/usuarios/:id', (request, res) =>{
+    const id = request.params.id
     const usuario = usuarios.find(u => u.id == id)
 
-    response.json(usuario)
+    if(usuario){
+        res.json(usuario)
+    }else{
+        res.status(404).json({mensagem: "Usuário não encontrado"})
+    }
+})
+
+app.get('/usuarios/nome/:nome', (req, res) => {
+    const buscaNome = req.params.nome.toLowerCase();
+    const resultados = usuarios.filter(u => u.nome.toLowerCase().includes(buscaNome));
+    if(resultados.length > 0){
+        res.json(resultados);
+    }else{
+        res.status(404).json({mensagem: "Usuário não encontrado"})
+    }
+
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+    const id = req.params.id
+    usuarios = usuarios.filter(u => u.id != id);
+
+    res.json({mensagem: "Usuário removido com sucesso"})
+})
+
+app.post('/usuarios', (req, res) => {
+    const novoUsuario = {
+        id: usuarios.length + 1,
+        nome: req.body.nome,
+        idade: req.body.idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario)
 })
